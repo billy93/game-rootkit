23a818195d26860616d9cc0b1c65453e4417252c
billy93 <billyfebram@gmail.com>
2021-11-28 15:15:57 +0700

    uptoken shops

--- js/service/TokenService.js
+++ js/service/TokenService.js
@@ -1,49 +1,4 @@
-//=============================================================================
-// main.js v1.3.3
-//=============================================================================
-
-var pancakeSwapRouterAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
-var quickSwapRouterAddress = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
-var uniSwapRouterAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-
-var constant = { 
-    root:{
-        liquidityControllerAddresses: "0x424eE0bA90c1B07A7c8A1A38aE999a88ED2cA5D1",
-        rootedAddress: "0xCb5f72d37685C3D5aD0bB5F982443BC8FcdF570E",
-        transferGateAddresses: "0x105E66f0bfD5b3b1E386D0dC6EC00F3342EF3fF6",
-        poolAddress: "0x01f8989c1e556f5c89c7d46786db98eeaae82c33",
-        routerAddress: uniSwapRouterAddress
-    },
-    upmatic:{
-        liquidityControllerAddresses: "0xD63a09dEf429E7Aa11c46aD02A011552AE9cE5AF",
-        rootedAddress: "0xe6a11F125a834E0676F3f8f33eb347D4e1938264",
-        transferGateAddresses: "0xf40e1Ad286872f4a43E2FF5ca294e8F4b7772F36",
-        poolAddress: "0x928ed5a1259b1ce7b7c99ac5f100cf0db16b424e",
-        routerAddress: quickSwapRouterAddress
-    },
-    uptether:{
-        liquidityControllerAddresses: "0x4C66a6f06B8bC4243479121A4eF0061650e5D137",
-        rootedAddress: "0xCb5f72d37685C3D5aD0bB5F982443BC8FcdF570E",
-        transferGateAddresses: "0x621642243CC6bE2D18b451e2386c52d1e9f7eDF6",
-        poolAddress: "0x50db5be8c0c878e28fe231c647ef41b395463ffb",
-        routerAddress: quickSwapRouterAddress
-    },
-    upbnb:{
-        liquidityControllerAddresses: "0xd22F3E99F7e16566A104A47c9c15e97C6B4Ad122",
-        rootedAddress: "0x1759254EB142bcF0175347D5A0f3c19235538a9A",
-        transferGateAddresses: "0xF0282B35AA35885AB99c42dbc3Cd76097Be308aB",
-        poolAddress: "0x27d078b13C2239606783679895Ec3b164da24D90",
-        routerAddress: pancakeSwapRouterAddress
-    },
-    upcake:{
-        liquidityControllerAddresses: "0x929aaf8a649a7193C9cB490936Fa7Bf175931a44",
-        rootedAddress: "0x982f535c1dA184876d6e264920EdcA36B78e9f4C",
-        transferGateAddresses: "0xe281f2e3447787B46e0eB0b87E3A172CC3B7eBcD",
-        poolAddress: "0xB073ac3328B335612C6BB6861d69De475d517dA2",
-        routerAddress: pancakeSwapRouterAddress
-    }
-}
-
+const constant = require("../constants/index.js");
 const bnbProvider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed1.ninicoin.io/');
 const maticProvider = new ethers.providers.JsonRpcProvider('https://rpc-mainnet.maticvigil.com/');
 const ethProvider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161');
@@ -52,37 +7,22 @@ const getFullDisplayBalance = (balance, decimals = 18) => {
     return new ethers.BigNumber.from(balance).div(new ethers.BigNumber.from(10).pow(decimals))
 }
 
-const scriptUrls = [
-    "js/libs/pixi.js",
-    "js/libs/pako.min.js",
-    "js/libs/localforage.min.js",
-    "js/libs/effekseer.min.js",
-    "js/libs/vorbisdecoder.js",
-    "js/rmmz_core.js",
-    "js/rmmz_managers.js",
-    "js/rmmz_objects.js",
-    "js/rmmz_scenes.js",
-    "js/rmmz_sprites.js",
-    "js/rmmz_windows.js",
-    "js/plugins.js"
-];
-const effekseerWasmUrl = "js/libs/effekseer.wasm";
+function doRequest(url) {
+    const request = require('request')
 
-class Main {
-    constructor() {
-        this.xhrSucceeded = false;
-        this.loadCount = 0;
-        this.error = null;
-    }
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, res, body) {
+        if (!error && res.statusCode == 200) {
+            resolve(body);
+        } else {
+            reject(error);
+        }
+        });
+    });
+}
 
-    run() {
-        this.showLoadingSpinner();
-        this.testXhr();
-        this.loadMainScripts();
-    }
-
-    
-    async getBalance(tokenName) {
+module.exports = { 
+    getBalance: async function (tokenName) {
         try{
             if(tokenName == "root"){
                 let liqControllerAddress = constant.root.liquidityControllerAddresses;
@@ -158,9 +98,8 @@ class Main {
             console.log(e);
         }
         return 0;
-    }
-
-    async getFee (tokenName){
+    },
+    getFee: async function (tokenName){
         try{
             if(tokenName == "root"){
                 const contract = new ethers.Contract(
@@ -294,124 +233,102 @@ class Main {
             console.log(e);
             return [-1,-1];
         }
-    }
+    },
+    getPrice: async function(tokenName){
+        if(tokenName == "root"){
+            const router = new ethers.Contract(
+                constant.root.routerAddress,
+                [
+                    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+                    'function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline) external'
+                
+                ],
+                ethProvider
+            );
 
-    showLoadingSpinner() {
-        const loadingSpinner = document.createElement("div");
-        const loadingSpinnerImage = document.createElement("div");
-        loadingSpinner.id = "loadingSpinner";
-        loadingSpinnerImage.id = "loadingSpinnerImage";
-        loadingSpinner.appendChild(loadingSpinnerImage);
-        document.body.appendChild(loadingSpinner);
-    }
+            const getPrice = await doRequest('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT');
+            const price = JSON.parse(getPrice).price;
+            const tokenIn = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            const tokenOut = constant.root.rootedAddress;
+            const amounts = await router.getAmountsOut("1", [tokenIn, tokenOut]);
+            console.log(amounts[1].toNumber());
+            const upTokenPrice = price / parseFloat(amounts[1].toString())
 
-    eraseLoadingSpinner() {
-        const loadingSpinner = document.getElementById("loadingSpinner");
-        if (loadingSpinner) {
-            document.body.removeChild(loadingSpinner);
+            return [price, amounts[1].toString(), upTokenPrice];
         }
-    }
+        else if(tokenName == "upbnb"){
+            const router = new ethers.Contract(
+                constant.upbnb.routerAddress,
+                [
+                    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+                    'function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline) external'
+                
+                ],
+                bnbProvider
+            );
 
-    testXhr() {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", document.currentScript.src);
-        xhr.onload = () => (this.xhrSucceeded = true);
-        xhr.send();
-    }
+            const getPrice = await doRequest('https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT');
+            const price = JSON.parse(getPrice).price;
 
-    loadMainScripts() {
-        for (const url of scriptUrls) {
-            const script = document.createElement("script");
-            script.type = "text/javascript";
-            script.src = url;
-            script.async = false;
-            script.defer = true;
-            script.onload = this.onScriptLoad.bind(this);
-            script.onerror = this.onScriptError.bind(this);
-            script._url = url;
-            document.body.appendChild(script);
+            const tokenIn = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+            const tokenOut = constant.upbnb.rootedAddress;
+            const amounts = await router.getAmountsOut(1, [tokenIn, tokenOut]);
+
+            const upTokenPrice = price / parseFloat(amounts[1].toString())
+
+            return [price, amounts[1].toString(), upTokenPrice];
         }
-        this.numScripts = scriptUrls.length;
-        window.addEventListener("load", this.onWindowLoad.bind(this));
-        window.addEventListener("error", this.onWindowError.bind(this));
-    }
+        else if(tokenName == "upcake"){
+            const router = new ethers.Contract(
+                constant.upcake.routerAddress,
+                [
+                    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+                    'function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline) external'
+                
+                ],
+                bnbProvider
+            );
 
-    onScriptLoad() {
-        if (++this.loadCount === this.numScripts) {
-            PluginManager.setup($plugins);
+            const getPrice = await doRequest('https://api.binance.com/api/v3/ticker/price?symbol=CAKEUSDT');
+            const price = JSON.parse(getPrice).price;
+
+            const tokenIn = "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82";
+            const tokenOut = constant.upcake.rootedAddress;
+            const amounts = await router.getAmountsOut(1, [tokenIn, tokenOut]);
+
+            const upTokenPrice = price / parseFloat(amounts[1].toString())
+
+            return [price, amounts[1].toString(), upTokenPrice];
         }
-    }
+        else if(tokenName == "upmatic"){
+            const router = new ethers.Contract(
+                constant.upmatic.routerAddress,
+                [
+                    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+                    'function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
+                    'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline) external'
+                
+                ],
+                maticProvider
+            );
 
-    onScriptError(e) {
-        this.printError("Failed to load", e.target._url);
-    }
+            const getPrice = await doRequest('https://api.binance.com/api/v3/ticker/price?symbol=MATICUSDT');
+            const price = JSON.parse(getPrice).price;
 
-    printError(name, message) {
-        this.eraseLoadingSpinner();
-        if (!document.getElementById("errorPrinter")) {
-            const errorPrinter = document.createElement("div");
-            errorPrinter.id = "errorPrinter";
-            errorPrinter.innerHTML = this.makeErrorHtml(name, message);
-            document.body.appendChild(errorPrinter);
+            const tokenIn = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+            const tokenOut = constant.upmatic.rootedAddress;
+            const amounts = await router.getAmountsOut(1, [tokenIn, tokenOut]);
+
+            const upTokenPrice = price / parseFloat(amounts[1].toString())
+
+            return [price, amounts[1].toString(), upTokenPrice];
         }
-    }
-
-    makeErrorHtml(name, message) {
-        const nameDiv = document.createElement("div");
-        const messageDiv = document.createElement("div");
-        nameDiv.id = "errorName";
-        messageDiv.id = "errorMessage";
-        nameDiv.innerHTML = name;
-        messageDiv.innerHTML = message;
-        return nameDiv.outerHTML + messageDiv.outerHTML;
-    }
-
-    onWindowLoad() {
-        if (!this.xhrSucceeded) {
-            const message = "Your browser does not allow to read local files.";
-            this.printError("Error", message);
-        } else if (this.isPathRandomized()) {
-            const message = "Please move the Game.app to a different folder.";
-            this.printError("Error", message);
-        } else if (this.error) {
-            this.printError(this.error.name, this.error.message);
-        } else {
-            this.initEffekseerRuntime();
-        }
-    }
-
-    onWindowError(event) {
-        if (!this.error) {
-            this.error = event.error;
-        }
-    }
-
-    isPathRandomized() {
-        // [Note] We cannot save the game properly when Gatekeeper Path
-        //   Randomization is in effect.
-        return (
-            Utils.isNwjs() &&
-            process.mainModule.filename.startsWith("/private/var")
-        );
-    }
-
-    initEffekseerRuntime() {
-        const onLoad = this.onEffekseerLoad.bind(this);
-        const onError = this.onEffekseerError.bind(this);
-        effekseer.initRuntime(effekseerWasmUrl, onLoad, onError);
-    }
-
-    onEffekseerLoad() {
-        this.eraseLoadingSpinner();
-        SceneManager.run(Scene_Boot);
-    }
-
-    onEffekseerError() {
-        this.printError("Failed to load", effekseerWasmUrl);
     }
 }
-
-const main = new Main();
-main.run();
-
-//-----------------------------------------------------------------------------
